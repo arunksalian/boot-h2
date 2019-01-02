4213ccdd1c3fcd1c3dbaa2e3448843240e210e71
Arun Kannan <arkannan@ghx.com>
2019-01-02 13:31:54 +0530

    Added jpa model

---
+++ src/main/java/com/slin/app/entity/Student.java
@@ -0,0 +1,18 @@
+package com.slin.app.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Student {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String name;
+	private String passportNumber;
+}
